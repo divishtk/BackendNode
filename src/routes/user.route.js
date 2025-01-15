14a332fbde6ece17ec1d2e32534cb5cb060c9045
divishtk <divisht.avion@hotmail.com>
2025-01-15 15:54:14 +0530

    added updation routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,30 +1,50 @@
 import { Router } from "express";
-import { loginUser, logoutuser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js"
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  loginUser,
+  logoutuser,
+  refreshAccessToken,
+  registerUser,
+  updateAccDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-
 router.route("/register").post(
-    upload.fields([{
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
 );
 
-
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutuser);
-router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/update-password").post(verifyJWT, changeCurrentPassword);
+router.route("/get-currentuser").post(verifyJWT, getCurrentUser);
+router.route("/update-account-details").post(verifyJWT, updateAccDetails);
+
+//for uploading single file
+router
+  .route("/update-avatar")
+  .post(upload.single("avatar"), verifyJWT, updateUserAvatar);
+router
+  .route("/update-coverImage")
+  .post(upload.single("coverImage"), verifyJWT, updateUserCoverImage);
 
 export default router;
